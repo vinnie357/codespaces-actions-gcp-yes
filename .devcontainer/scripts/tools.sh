3ed8404682d0282b0edec34894c11d8c0d85eb2c
Vinnie Mazza <vinnie.mazza@gmail.com>
2020-08-06 13:59:14 +0000

    approve the jq install duhdoy

--- .devcontainer/scripts/tools.sh
+++ .devcontainer/scripts/tools.sh
@@ -2,7 +2,7 @@
 # install tools for container standup
 echo "---getting tools---"
 sudo apt-get update
-sudo apt-get install jq
+sudo apt-get install -y jq
 . gcloud.sh
 . terraform.sh
 echo "---tools done---"
